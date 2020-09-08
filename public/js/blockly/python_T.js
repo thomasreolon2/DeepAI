10ae1bb66c5519560cb5ea2c16b320acafa21235
명왕성 <50352637+pluton126@users.noreply.github.com>
2020-09-08 18:39:06 +0900

    1.서버 2. 시각화 3. numpy 수정 4.기본블록 수정

---
+++ public/js/blockly/python_T.js
@@ -0,0 +1,630 @@
+Blockly.defineBlocksWithJsonArray(
+    [
+  // 라이브러리 임포트 전체  
+  {
+    "type": "lib_import_a",
+    "message0": "( 라이브러리 임포트 ) %1 %2  :  from 여부 %3",
+    "args0": [
+      {
+        "type": "field_dropdown",
+        "name": "Lib_import_select",
+        "options": [
+          [
+            "numpy",
+            "Lib_import_nump"
+          ],
+          [
+            "pandas",
+            "Lib_import_panda"
+          ],
+          [
+            "matplotlib",
+            "Lib_import_matplotlib"
+          ],
+          [
+            "scikit-learn",
+            "Lib_import_scikit"
+          ]
+        ]
+      },
+      {
+        "type": "input_dummy"
+      },
+      { 
+        "type": "field_checkbox",
+        "name": "Lib_check_from",
+        "checked": false
+      }
+    ],
+    "inputsInline": true,
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 125,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  // 라이브러리 단일 임포트 부분    
+  {
+    "type": "lib_import",
+    "message0": "( 라이브러리 임포트 )  import %1",
+    "args0": [ 
+      {
+        "type": "field_input",
+        "name": "Lib_import_name",
+        "text": ""
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 125,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  // 라이브러리  임포트 as
+  { 
+    "type": "lib_import_as",
+    "message0": "( 라이브러리 임포트 )  import %1  as %2",
+    "args0": [
+      {
+        "type": "field_input",
+        "name": "Lib_import_name_1",
+        "text": ""
+      },
+      {
+        "type": "field_input",
+        "name": "Lib_import_name_second",
+        "text": ""
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 125,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  // 라이브러리  임포트 from
+  {
+    "type": "lib_import_from",
+    "message0": "( 라이브러리 임포트 ) from %1 import %2", 
+    "args0": [
+      {
+        "type": "field_input",
+        "name": "Lib_import_name_from",
+        "text": ""
+      },
+      {
+        "type": "field_input",
+        "name": "Lib_import2",
+        "text": ""
+      }
+    ], 
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 125,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  
+  //print 하기   
+  {
+    "type": "print",
+    "message0": "print %1 %2",
+    "args0": [ 
+      {
+        "type": "input_dummy"
+      },
+      { 
+        "type": "input_value",
+        "name": "print_value"
+      }
+    ],
+    "inputsInline": true,
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 125,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  //numpy
+  {
+    "type": "import_numpy",
+    "message0": "%1",
+    "args0": [
+      {
+        "type": "field_input",
+        "name": "Import_numpy_n",
+        "text": "numpy"
+      }
+    ],
+    "output": null,
+    "colour": 125,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  //배열 생성
+  {
+    "type": "create_arry",
+    "message0": "배열 생성 %1",
+    "args0": [  
+      {
+        "type": "field_input", 
+        "name": "Create_arry_val",
+        "text": ""
+      }
+    ],
+    "inputsInline": true,
+    "output": null,
+    "colour": 125,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  //배열 곱 
+  {
+    "type": "arry_mul",
+    "message0": "배열 곱 %1 %2 * %3",
+    "args0": [
+      {
+        "type": "input_dummy"
+      },
+      {
+        "type": "input_value",
+        "name": "Arry_mul_first"
+      },
+      { 
+        "type": "input_value",
+        "name": "Arry_mul_second"
+      }
+    ],
+    "inputsInline": true,
+    "previousStatement": null,
+    "nextStatement": null, 
+    "colour": 125,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  // 배열 나누기 
+  { 
+    "type": "arry_div",
+    "message0": "배열 나누기 %1 %2 / %3",
+    "args0": [
+      {
+        "type": "input_dummy"
+      },
+      {
+        "type": "input_value",
+        "name": "Arry_div_first"
+      },
+      {
+        "type": "input_value",
+        "name": "Arry_div_second"
+      }
+    ],
+    "inputsInline": true,
+    "previousStatement": null,
+    "nextStatement": null, 
+    "colour": 125,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  //배열 합
+  {
+    "type": "arry_plus",
+    "message0": "배열 합 %1 %2 + %3",
+    "args0": [
+      {
+        "type": "input_dummy"
+      },
+      {
+        "type": "input_value",
+        "name": "Arry_plus_first"
+      },
+      {
+        "type": "input_value",
+        "name": "Arry_plus_second"
+      }
+    ],
+    "inputsInline": true,
+    "previousStatement": null,
+    "nextStatement": null, 
+    "colour": 125,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  //배열 뺴기
+  {
+    "type": "arry_minus",
+    "message0": "배열 빼기 %1 %2 - %3",
+    "args0": [
+      {
+        "type": "input_dummy"
+      },
+      {
+        "type": "input_value",
+        "name": "Arry_Minus_first"
+      },
+      {
+        "type": "input_value",
+        "name": "Arry_Minus_second"
+      }
+    ],
+    "inputsInline": true,
+    "previousStatement": null,
+    "nextStatement": null, 
+    "colour": 125,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+    // 배열 np.arry 
+  { 
+    "type": "np_array",
+    "message0": "np.arry %1 %2",
+    "args0": [
+      {
+        "type": "input_dummy"
+      },
+      { 
+        "type": "input_value",
+        "name": "np_array_input"
+      }
+    ],
+    "inputsInline": true,
+    "output": null,
+    "colour": 125,
+    "tooltip": "",
+    "helpUrl": ""
+  }, 
+  // numpy 함수 zeros
+  {
+    "type": "numpy_zeros",
+    "message0": "np.zeros %1 , dtype 사용여부(정수) %2",
+    "args0": [
+      {
+        "type": "field_input",
+        "name": "Numpy_zeros_num", 
+        "text": "" 
+      },
+      {  
+        "type": "field_checkbox",  
+        "name": "Numpy_zeros_check_dtype", 
+        "checked": false
+      }
+    ],
+    "inputsInline": true,
+    "output": null,
+    "colour": 125,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  // np.full 배열 생성 및 , 채워질 값 
+  {
+    "type": "numpy_full",
+    "message0": "np.full ( 배열 생성 ) %1 채워질 값 %2",
+    "args0": [
+      {
+        "type": "field_input",
+        "name": "Numpy_full_pre_val",
+        "text": ""
+      },
+      {
+        "type": "field_input",
+        "name": "Numpy_full_next_val",
+        "text": ""
+      }
+    ],
+    "inputsInline": true,
+    "output": null,
+    "colour": 125,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  // eye 함수 대각선으로 1채우기 
+  {
+    "type": "numpy_eye",
+    "message0": "np.eye ( 배열 대각선 1 로 채우기 ) -> 배열생성 : %1",
+    "args0": [
+      { 
+        "type": "field_input", 
+        "name": "Numpy_eye_arry",
+        "text": ""
+      }
+    ],
+    "inputsInline": true,
+    "output": null,
+    "colour": 125,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  // 최대값 찾기 
+  {
+    "type": "numpy_max",
+    "message0": "max ( 최대 값 찾기 )  %1",
+    "args0": [ 
+      {
+        "type": "field_input", 
+        "name": "Numpy_max_arry",
+        "text": ""
+      }
+    ],
+    "inputsInline": true, 
+    "previousStatement": null, 
+    "nextStatement": null,
+    "colour": 125,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  //최소 값 찾기 
+  {
+    "type": "numpy_min",
+    "message0": "np.min ( 최소 값 찾기 ) %1",
+    "args0": [
+      {
+        "type": "field_input",
+        "name": "Numpy_min_arry", 
+        "text": ""
+      }
+    ],
+    "inputsInline": true,
+    "previousStatement": null, 
+    "nextStatement": null,
+    "colour": 125,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  //shape 행열 차원 반환 
+  {
+    "type": "numpy_shape",
+    "message0": "변수이름 : %1 .shape ( 행렬 차원 반환) ",
+    "args0": [
+      {
+        "type": "field_input", 
+        "name": "Numpy_shape", 
+        "text": ""
+      }
+    ],
+    "inputsInline": true,
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 125,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  // reshpae 행렬 차원 변경 
+  {
+    "type": "numpy_reshape",
+    "message0": "변수이름 : %1 .reshape ( 행렬 차원 변경)   값 : %2",
+    "args0": [
+      {
+        "type": "field_input",
+        "name": "Numpy_reshape_vari",
+        "text": ""
+      },
+      {
+        "type": "field_input",
+        "name": "Numpy_reshape_array",
+        "text": ""
+      }
+    ],
+    "inputsInline": true,
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 125,
+    "tooltip": "",
+    "helpUrl": ""
+  }, 
+  // random.randn  표준정규 분포에 따른 무작위 숫자 생성 
+  {
+      "type": "numpy_random_rand",
+      "message0": "np.random ( randn : 정규분포 난수 , randint :정수 난수)   값 : %1 %2",
+      "args0": [
+        {
+          "type": "field_dropdown",
+          "name": "Numpy_Get_random",  
+          "options": [
+            [
+              "randn",
+              "Numpy_random_randn"
+            ],
+            [
+              "randint",
+              "Numpy_random_randint"
+            ]
+          ]
+        },
+        {
+          "type": "field_input",
+          "name": "Numpy_random_rand_array",
+          "text": ""
+        }
+      ],
+      "inputsInline": true, 
+      "output": null,
+      "colour": 125,
+      "tooltip": "", 
+      "helpUrl": ""
+    },
+    // 지수 exp
+    { 
+      "type": "numpy_exp",
+      "message0": " %1 np.exp ( 지수 함수 ) %2", 
+      "args0": [
+        {
+          "type": "input_dummy"
+        },
+        { 
+          "type": "input_value",
+          "name": "Numpy_exp_val"
+        }
+      ],
+      "inputsInline": true, 
+      "output": null,
+      "colour": 125,
+      "tooltip": "",
+      "helpUrl": ""
+    },
+  //matplot1 
+  {
+    "type": "matplotlib_main",
+    "message0": "시각화 틀 ",
+    "inputsInline": true,
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 125,
+    "tooltip": "", 
+    "helpUrl": ""
+  }, 
+  //matplot2 
+  {
+    "type": "matplotlib_pre_graph",
+    "message0": "시각화 준비 %1  |   ( X , Y ) 값 : %2  |  그래프 종류 : %3  |  형태 (선 그래프만 해당) 사용 여부: %4 %5",
+    "args0": [
+      {
+        "type": "field_dropdown",
+        "name": "matplotlib_num_location",
+        "options": [
+          [
+            "전체 표시",
+            "matplotlib_screen"
+          ],
+          [
+            "1번_표시",
+            "matplotlib_num_1"
+          ],
+          [
+            "2번_표시",
+            "matplotlib_num_2"
+          ],
+          [
+            "3번_표시",
+            "matplotlib_num_3"
+          ],
+          [
+            "4번_표시",
+            "matplotlib_num_4"
+          ]
+        ]
+      },
+      {
+        "type": "field_input",
+        "name": "matplotlib_user_xy",
+        "text": ""
+      },
+      {
+        "type": "field_dropdown",
+        "name": "matplotlib_graph_select",
+        "options": [
+          [
+            "선_그래프",
+            "matplotlib_line"
+          ],
+          [
+            "Scatter_그래프",
+            "matplotlib_scatter"
+          ],
+          [
+            "막대_그래프",
+            "matplotlib_bar"
+          ],
+          [
+            "에러_바",
+            "matplotlib_error_bar"
+          ],
+          [
+            "박스_그래프",
+            "matplotlib_box"
+          ],
+          [
+            "히스토그램",
+            "matplotlib_histogram"
+          ]
+        ]
+      },
+      {
+        "type": "field_checkbox",
+        "name": "matplotlib_line_OX",
+        "checked": false
+      },
+      {
+        "type": "field_dropdown",
+        "name": "matplotlib_line_select",
+        "options": [
+          [
+            "선",
+            "matplotlib_line_1"
+          ],
+          [
+            "점",
+            "matplotlib_line_2"
+          ],
+          [
+            "점_선",
+            "matplotlib_line_3"
+          ], 
+          [
+            "x_표시",
+            "matplotlib_line_4"
+          ]
+        ]
+      }
+    ],
+    "inputsInline": true,
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 125,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  
+  //matplot3 
+  {
+    "type": "matplotlib_graph_end",
+    "message0": "시각화 하기 ",
+    "inputsInline": true,
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 125,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  
+  //선형 회귀 생성 
+  {
+    "type": "linearregression_c",
+    "message0": "선형 회귀 생성",
+    "inputsInline": true,
+    "output": null,
+    "colour": 125,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  //모델 훈련 
+  {
+    "type": "linearregression_train",
+    "message0": "변수 : %1 .fit 모델 훈련 -> X : %2 Y : %3", 
+    "args0": [
+      {
+        "type": "input_value",
+        "name": "LinearRegression_val"
+      },
+      {
+        "type": "field_input",
+        "name": "LinearRegression_X",
+        "text": ""
+      },
+      { 
+        "type": "field_input",
+        "name": "LinearRegression_Y",
+        "text": ""
+      }
+    ],
+    "inputsInline": true,
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 125,
+    "tooltip": "",
+    "helpUrl": ""
+  },
+  
+  
+  
+  ]
+  ); 
--- server.js
+++ server.js
@@ -1,63 +1,96 @@
 'use strict';
 
+
 var express = require('express');
 var fs = require('fs');
-var https = require('https');
-const ejs = require("ejs");
-const bodyParser = require('body-parser');          //
+//var https = require('https');
+const ejs = require("ejs"); 
 
-var app = express();
 
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: false}));  // URL 인코딩 안함 
-app.use(bodyParser.json());                         // json 타입으로 파싱하게 설정
-
-// js & css
-app.use('/js', express.static(__dirname + '/public/js'));
-app.use(express.static(__dirname + '/public'));
+//크롤링 
+var client = require('cheerio-httpcli');  
+const bodyParser = require('body-parser'); 
+var urlTpye = require('url');   
+var request = require('request');
+var fname ;
+//크롤링 
 
 const options = {
-	key: fs.readFileSync('./keys/private.pem'),
-	cert: fs.readFileSync('./keys/public.pem')
+    key: fs.readFileSync('./keys/private.pem'),
+    cert: fs.readFileSync('./keys/public.pem')
 };
 
-https.createServer(options, app).listen(8000, function() {
-    console.log("HTTPS server listening on port " + 8000);
-  });
 
-// index
+var app = express();
+var server = require('https').createServer(options, app);
+var io = require('socket.io')(server);// http server를 socket.io server로 upgrade 
+ 
+app.set("view engine", "ejs"); 
+app.use(bodyParser.urlencoded({extended: false})); // URL 인코딩 안함
+app.use(bodyParser.json()); // json 타입으로 파싱하게 설정
+
+// js & css 
+app.use('/js', express.static(__dirname + '/public/js')); 
+app.use(express.static(__dirname + '/public'));
+
+server.listen(8000, function () {
+    console.log('Socket IO server listening on port 8000');
+}); 
+ 
+//index 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + "/BlockExport_Editor.html");
-});
+}); 
 
-//AJAX GET METHOD
-/* Deprecated 
-app.get('/code_run',function(req,res) {
-    var data = req.query.data;
-    console.log('GET Parameter = ' + data);
-    var result = data;
-    console.log(result);
-    res.send({result:result});
-});
-*/
-
-// cnn test
 app.get('/cnn', function (req, res) {
     res.sendFile(__dirname + "/cnnTest.html");
 });
 
-app.get("/test", function(req, res){   // postTest라는 주소로 POST요청이 들어오면 실행
+app.get("/test", function (req, res) { // postTest라는 주소로 POST요청이 들어오면 실행
     res.render(__dirname + "/test.ejs");
-});
+}); 
 
-// POST Test
-app.post("/postTest", function(req, res){   // postTest라는 주소로 POST요청이 들어오면 실행
+
    
-    console.log(req.body);      // body에 있는 정보를 콘솔창에 출력.
-    res.json({ok:true});        // 클라이언트에 성공했다고 신호를 보냄.
-    
-});
+app.get('/show_csv', function (req, res) {  //csv 파일 읽기 
+  res.sendFile(__dirname + "/show_csv.html");
+});      
 
-//app.listen(8000, function () {
-//    console.log("hello!");
-//});
+app.get('/show_chart', function (req, res) {  //차트 보기 
+  res.sendFile(__dirname + "/show_chart.html");
+});   
+ 
+// POST Test 
+app.post("/postTest", function (req, res) { // postTest라는 주소로 POST요청이 들어오면 실행
+
+    console.log(req.body); // body에 있는 정보를 콘솔창에 출력.  
+    res.json({ok: true}); // 클라이언트에 성공했다고 신호를 보냄.
+
+});  
+ 
+
+
+
+//socket io 부분 
+// connection이 수립되면 event handler function의 인자로 socket인 들어온다
+io.on('connection', function (socket) { 
+ 
+
+    socket.on('Scripts_Block', function (data) {
+      console.log(data.S_option);  
+      console.log(data.S_keyword); 
+      console.log(data.S_num); 
+      console.log(data.S_local); 
+      scripts(data.S_option,data.S_keyword,data.S_num,data.S_local); 
+     }); 
+ 
+    // force client disconnect from server
+    socket.on('forceDisconnect', function () {  
+        socket.disconnect();   
+    })
+
+    socket.on('disconnect', function () {  
+        console.log('user disconnected: ' + socket.name);
+    });
+ 
+});  
