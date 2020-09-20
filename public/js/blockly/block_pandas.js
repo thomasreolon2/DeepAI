c184fcedbae19cdc23598cfcf5aca3ff93e16b27
y12s23k <70931309+y12s23k@users.noreply.github.com>
2020-09-21 06:10:47 +0900

    판다스블록 색깔변경

--- public/js/blockly/block_pandas.js
+++ public/js/blockly/block_pandas.js
@@ -79,7 +79,7 @@ Blockly.Blocks['normal'] = {
       .appendField(")");
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
-    this.setColour("%{BKY_SCIKITLEARN_HUE}");
+    this.setColour(pandas_color);
     this.setTooltip("");
     this.setHelpUrl("");
   }
