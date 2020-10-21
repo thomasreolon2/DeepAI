2c27743c68f4553319a394f0c0447894d24eb91a
Jeong <dinb1242@naver.com>
2020-10-21 13:51:47 +0900

    test.js, client_id?

--- test.js
+++ test.js
@@ -1,16 +1,10 @@
-    var x,w,b,lr,epochs,w_gradient,b_gradient;
+const http = require('http');
+const hostname = '114.71.137.154';
+const port = 8000;
 
-    x_data=nj.array([2,4,6,8,10,12,14]);
-    y_data=nj.array([0,0,0,1,1,1,1]);
-    w=0;
-    b=0;
-    lr=0.001
-    epochs=100
-    function sigmoid(x){
-        return 1/(1+nj.exp **(-x))   
-    }
-
-    for(var i=0;i<epochs; i++ ){
-        w_gradient=x_data.multiply(sigmoid(x_data.multiply(w).add(b)))
-
-    }
+http.createServer((req, res)=>{
+    res.write('Hello');
+    res.end('bye');
+}).listen(port,hostname, ()=>{
+    console.log(`Server running at http://${hostname}:${port}`);
+})
