2a2079de9f4d39d7032cbb5d32ebcdc6e5003f76
Bantree96 <51081373+Bantree96@users.noreply.github.com>
2020-12-22 13:46:47 +0900

    포트 로그도 변경

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const options = {
 
 var app = express();
 var server = require('http').createServer(options, app).listen(18081, function(){
-    console.log('Socket IO server listening on port 8000');
+    console.log('Socket IO server listening on port 18081');
 });
 var io = require('socket.io')(server);// http server를 socket.io server로 upgrade 
  
