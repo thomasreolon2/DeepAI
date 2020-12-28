2337cd77c056940ef9bd5c1b51e67f613ef95aba
양승국 <y12s23k@naver.com>
2020-12-29 00:38:26 +0900

    ㅇㅇ

--- public/js/blockly/block_static_py.js
+++ public/js/blockly/block_static_py.js
@@ -13,7 +13,7 @@ Blockly.Python['statistics1'] = function (block) {
   var value_name = Blockly.Python.valueToCode(block, 'va', Blockly.Python.ORDER_ATOMIC);
   var dropdown_name = block.getFieldValue('NAME');
   // TODO: Assemble JavaScript into code variable.
-  var code = dropdown_name + '(' + value_name + ')\n';
+  var code = dropdown_name + '(' + value_name + ')';
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.Python.ORDER_ATOMIC];
 };
