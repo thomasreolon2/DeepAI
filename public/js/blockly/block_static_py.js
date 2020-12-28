3aac0ad4a215d0fa03f3061fa6e46aaf179ccca1
양승국 <y12s23k@naver.com>
2020-12-29 01:46:17 +0900

    랜덤함수 완

--- public/js/blockly/block_static_py.js
+++ public/js/blockly/block_static_py.js
@@ -8,7 +8,7 @@ Blockly.Python['static_library'] = function (block) {
   return code;
 };
 
-//2020-09-19 양승국 수정
+
 Blockly.Python['statistics1'] = function (block) {
   var value_name = Blockly.Python.valueToCode(block, 'va', Blockly.Python.ORDER_ATOMIC);
   var dropdown_name = block.getFieldValue('NAME');
@@ -19,7 +19,6 @@ Blockly.Python['statistics1'] = function (block) {
 };
 
 
-//2020-09-19 양승국 수정
 Blockly.Python['statistics2'] = function (block) {
   var value_name1 = Blockly.Python.valueToCode(block, 'va1', Blockly.Python.ORDER_ATOMIC);
   var value_name2 = Blockly.Python.valueToCode(block, 'va2', Blockly.Python.ORDER_ATOMIC);
@@ -29,6 +28,37 @@ Blockly.Python['statistics2'] = function (block) {
   else if (dropdown_name == 2) var code = "quantiles" + '(' + value_name1 + ", " + value_name2 + ')';
   else if (dropdown_name == 3) var code = "pvariance" + '(' + value_name1 + ", " + value_name2 + ')';
   else if (dropdown_name == 4) var code = "variance" + '(' + value_name1 + ", " + value_name2 + ')';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.Python.ORDER_ATOMIC];
+};
+
+////////////////////////////////////
+/// random 블록
+////////////////////////////////////
+
+Blockly.Python['random_library'] = function (block) {
+  // TODO: Assemble JavaScript into code variable.
+  var code = "import random\n";
+  return code;
+};
+
+Blockly.Python['random1'] = function (block) {
+  var value_name = Blockly.Python.valueToCode(block, 'va', Blockly.Python.ORDER_ATOMIC);
+  var dropdown_name = block.getFieldValue('NAME');
+  // TODO: Assemble JavaScript into code variable.
+  var code = "random." + dropdown_name + '(' + value_name + ')';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.Python.ORDER_ATOMIC];
+};
+
+
+Blockly.Python['random2'] = function (block) {
+  var value_name1 = Blockly.Python.valueToCode(block, 'va1', Blockly.Python.ORDER_ATOMIC);
+  var value_name2 = Blockly.Python.valueToCode(block, 'va2', Blockly.Python.ORDER_ATOMIC);
+  var dropdown_name = block.getFieldValue('NAME');
+  // TODO: Assemble JavaScript into code variable.
+  if (dropdown_name == 1)   var code = "random.randint" + '(' + value_name1 + ", " + value_name2 + ')';
+  else if (dropdown_name == 2) var code = "random.sample" + '(' + value_name1 + ", " + value_name2 + ')';
   // TODO: Change ORDER_NONE to the correct strength.
   return [code, Blockly.Python.ORDER_ATOMIC];
 };
