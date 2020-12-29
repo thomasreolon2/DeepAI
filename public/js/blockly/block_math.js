ec159519ea89063a96f55d1171ee039a89074b07
kingwoojin <74906939+kingwoojin@users.noreply.github.com>
2020-12-29 20:45:23 +0900

    math 수정 2

--- public/js/blockly/block_math.js
+++ public/js/blockly/block_math.js
@@ -19,9 +19,9 @@ Blockly.Blocks['math_library'] = {
     init: function () {
       this.appendDummyInput()
         .appendField("수학 1 ")
-        .appendField(new Blockly.FieldDropdown([["올림", "ceil"], ["절대값", "fabs"], ["계승 정수", "factorial"], ["내림", "floor"], 
-        ["정수 제곱근", "isqrt"], ["소수,정수", "modf"], ["e 거듭 제곱", "exp"], ["log10", "log10"], ["제곱근", "sqrt"], ["라디안에서 도 변환", "degrees "],
-        ["도에서 라디안 변환", "radians"], ["아크 코사인", "acos"], ["아크 사인", "asin"], ["아크 탄젠트", "atan"],["코사인", "cos"],["사인", "sin"],["탄젠트", "tan"]]), "NAME")
+        .appendField(new Blockly.FieldDropdown([["올림 (ceil)", "ceil"], ["절대값 (fabs)", "fabs"], ["계승 정수 (factorial)", "factorial"], ["내림 (floor)", "floor"], 
+        ["정수 제곱근 (isqrt)", "isqrt"], ["소수,정수 (modf)", "modf"], ["e 거듭 제곱 (exp)", "exp"], ["로그10 (log10)", "log10"], ["제곱근 (sqrt)", "sqrt"], ["라디안에서 도 변환 (degress)", "degrees "],
+        ["도에서 라디안 변환 (radians)", "radians"], ["아크 코사인 (acos)", "acos"], ["아크 사인 (asin)", "asin"], ["아크 탄젠트 (atan)", "atan"],["코사인 (cos)", "cos"],["사인 (sin)", "sin"],["탄젠트 (tan)", "tan"]]), "NAME")
         .appendField("(");
       this.appendValueInput("va")
         .setCheck(null)
@@ -39,7 +39,7 @@ Blockly.Blocks['math_library'] = {
     init: function () {
       this.appendDummyInput()
         .appendField("수학 2 ")
-        .appendField(new Blockly.FieldDropdown([["최대 공약수", "1"], ["자연로그", "2"], ["거듭제곱", "3"], ["유클리드 거리", "4"]]), "NAME")
+        .appendField(new Blockly.FieldDropdown([["최대 공약수 (gcd)", "1"], ["자연로그 (log)", "2"], ["거듭제곱 (pow)", "3"], ["유클리드 거리 (dist)", "4"]]), "NAME")
         .appendField("(");
       this.appendValueInput("va1")
         .setCheck(null);
@@ -59,7 +59,7 @@ Blockly.Blocks['math_library'] = {
     init: function () {
       this.appendDummyInput()
         .appendField("수학 3 ")
-        .appendField(new Blockly.FieldDropdown([["π", "pi"], ["e", "e"], ["양의 무한대", "inf"], ["nan 값", "nan"]]), "NAME")
+        .appendField(new Blockly.FieldDropdown([["파이 (π)", "pi"], ["자연로그 밑 (e)", "e"], ["양의 무한대 (inf)", "inf"], ["nan 값 (nan)", "nan"]]), "NAME")
       this.setInputsInline(true);
       this.setOutput(true, null);
       this.setColour(pandas_color);
