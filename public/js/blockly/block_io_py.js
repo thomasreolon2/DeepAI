e9bd713812b0d9e45b16b506ca92dbf206612756
Seongju-Lee <67941526+Seongju-Lee@users.noreply.github.com>
2021-01-01 22:29:44 +0900

    교수님 처리해주신 파일 부분 출력 수정
    파일 출력 결과 숟가락 얻기입니당!_!
    ㅎ_ㅎ

--- public/js/blockly/block_io_py.js
+++ public/js/blockly/block_io_py.js
@@ -46,7 +46,7 @@ Blockly.Python['fd_open'] = function (block) {
   var variable_list = Blockly.Python.variableDB_.getName(block.getFieldValue('list'), Blockly.Variables.NAME_TYPE);
   var value_text = Blockly.Python.valueToCode(block, 'text', Blockly.Python.ORDER_ATOMIC);
   // TODO: Assemble Python into code variable.
-  var code = variable_list + ' = open( "' + value_text  + '", "' + dropdown_set_menu + '" )\n';
+  var code = variable_list + ' = open( ' + value_text  + ', "' + dropdown_set_menu + '" )\n';
   return code;
 };
 
