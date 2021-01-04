c2847b820efa74e5cc34f27b7f068a78afcbee4b
kingwoojin <74906939+kingwoojin@users.noreply.github.com>
2021-01-05 02:42:38 +0900

    ser

---
+++ public/js/blockly/woojin.js
@@ -0,0 +1,94 @@
+Blockly.Blocks['sr_unique'] = {
+    init: function () {
+      this.appendDummyInput().appendField(Blockly.Msg.PANDAS_CORR_INQUIRY);
+      this.appendValueInput("data").setCheck(null)
+      this.setColour(pandas_color);
+      this.setInputsInline(true);
+      this.setOutput(true, null);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
+
+Blockly.Blocks['sr_name'] = {
+    init: function () {
+      this.appendDummyInput().appendField(Blockly.Msg.PANDAS_CORR_INQUIRY);
+      this.appendValueInput("data").setCheck(null)
+      this.setColour(pandas_color);
+      this.setInputsInline(true);
+      this.setOutput(true, null);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
+
+Blockly.Blocks['sr_is_in'] = {
+    init: function () {
+      this.appendDummyInput().appendField(Blockly.Msg.PANDAS_CORR_INQUIRY);
+      this.appendValueInput("data").setCheck(null)
+      this.appendValueInput("data2").setCheck(null)
+      this.setColour(pandas_color);
+      this.setInputsInline(true);
+      this.setOutput(true, null);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
+
+  Blockly.Blocks['sr_tolist'] = {
+    init: function () {
+      this.appendDummyInput().appendField(Blockly.Msg.PANDAS_CORR_INQUIRY);
+      this.appendValueInput("data").setCheck(null)
+      this.setColour(pandas_color);
+      this.setInputsInline(true);
+      this.setOutput(true, null);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+  };
+
+  Blockly.Blocks['sr_index()'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField(new Blockly.FieldVariable("index"), "1")
+          .appendField("(")
+          this.appendValueInput("index,list").setCheck(null)
+          .appendField(")");
+      this.setInputsInline(true);
+      this.setOutput(true, null);
+      this.setColour(pandas_color);
+   this.setTooltip("");
+   this.setHelpUrl("");
+    }
+  };
+
+
+  Blockly.Blocks['sr_index[]'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField(new Blockly.FieldVariable("index"), "1")
+          .appendField("(")
+          this.appendValueInput("색인").setCheck(null)
+          .appendField(")");
+      this.setInputsInline(true);
+      this.setOutput(true, null);
+      this.setColour(pandas_color);
+   this.setTooltip("");
+   this.setHelpUrl("");
+    }
+  };
+
+  Blockly.Blocks['sr_condition[]'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField(new Blockly.FieldVariable("condition"), "1")
+          .appendField("(")
+          this.appendValueInput("논리연산").setCheck("Boolean")
+          .appendField(")");
+      this.setInputsInline(true);
+      this.setOutput(true, null);
+      this.setColour(pandas_color);
+   this.setTooltip("");
+   this.setHelpUrl("");
+    }
+  };
---
+++ public/js/blockly/woojin_js.js
@@ -0,0 +1,34 @@
+Blockly.JavaScript['sr_unique'] = function(block) {
+    var code = '\n';
+    return code;
+  };
+
+Blockly.JavaScript['sr_name'] = function(block) {
+    var code = '\n';
+    return code;
+  };
+
+Blockly.JavaScript['sr_is_in'] = function(block) {
+    var code = '\n';
+    return code;
+  };
+
+Blockly.JavaScript['sr_tolist'] = function(block) {
+    var code = '\n';
+    return code;
+  };
+
+  Blockly.JavaScript['sr_index()'] = function(block) {
+    var code = '\n';
+    return code;
+  };
+
+  Blockly.JavaScript['sr_index[]'] = function(block) {
+    var code = '\n';
+    return code;
+  };
+
+  Blockly.JavaScript['sr_condition[]'] = function(block) {
+    var code = '\n';
+    return code;
+  };
