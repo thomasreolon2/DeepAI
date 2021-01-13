7a6f79a6192c3f0845ab1aef3618208323ba8e8c
남지원 <51081373+Bantree96@users.noreply.github.com>
2021-01-13 22:12:06 +0900

    사이킷이미지 이미지추가

--- public/js/blockly/block_skimage.js
+++ public/js/blockly/block_skimage.js
@@ -16,20 +16,63 @@ Blockly.Blocks['import_skimage'] = {
 Blockly.Blocks['skimage_data'] = {
   init: function () {
     this.appendDummyInput()
-        .appendField("[이미지사용]")
-        .appendField(new Blockly.FieldDropdown([
-          [
-              "카메라", "CAMERA"
-          ],
-          [
-              "ABC", "getvalues"
-          ],
-          [
-              "키,값쌍얻기", "items"
-          ],
-          [
-              "키,값쌍모두지우기", "clear"
-          ]
+      .appendField("[이미지사용]")
+      .appendField(new Blockly.FieldDropdown([
+        [
+          "우주인", "astronaut"  
+        ],
+        
+        [
+          "binary_blobs", "binary_blobs"
+        ],
+        // [
+        //   "벽돌", "brick"
+        // ],
+        // [
+        //   "색상원", "colorwheel"
+        // ],
+        [
+          "카메라", "camera"
+        ],
+        // [
+        //   "고양이", "cat"
+        // ],
+        [
+          "체스판", "checkerboard"
+        ],
+        [
+          "clock", "clock"
+        ],
+        [
+          "커피", "coffee"
+        ],
+        [
+          "동전", "coins"
+        ],
+        // [
+        //   "독수리", "eagle"
+        // ],
+        // [
+        //   "잔디", "grass"
+        // ],
+        // [
+        //   "자갈", "gravel"
+        // ],
+        [
+          "말", "horse"
+        ],
+        [
+          "로고", "logo"
+        ],
+        [
+          "책(페이지)", "page"
+        ],
+        [
+          "글자", "text"
+        ],
+        [
+          "로켓", "rocket"
+        ]
       ]), "DATA");
     this.setInputsInline(true);
     this.setOutput(true, null);
--- public/js/blockly/block_skimage_py.js
+++ public/js/blockly/block_skimage_py.js
@@ -8,14 +8,7 @@ Blockly.Python['import_skimage'] = function (block) {
 // 2021.01.13 이미지 사용 (남지원)
 Blockly.Python['skimage_data'] = function (block) {
   var dropdown_dic = block.getFieldValue('DATA');
-  if (dropdown_dic == 'CAMERA') {
-    var code = `data.camera()`;
-  } else if (dropdown_dic == 'clear') {
-    var code = variable_list + '.clear()';
-  } else if (dropdown_dic == 'getvalues') {
-      var code = variable_list + '.values()';
-  } else if (dropdown_dic == 'getkeys') {
-      var code = variable_list + '.keys()';
-  }
+  var code = `data.${dropdown_dic}()`;
+  
   return [code, Blockly.Python.ORDER_ATOMIC];
 };
