e0fc7e48bc7712abbf724197b36fd9735133a5ec
남지원 <51081373+Bantree96@users.noreply.github.com>
2021-01-21 23:11:43 +0900

    맷플롯립 수정본

--- public/js/blockly/jiwon_js.js
+++ public/js/blockly/jiwon_js.js
@@ -167,6 +167,16 @@ Blockly.JavaScript['set_xticklabels'] = function (block) {
   return [code, Blockly.JavaScript.ORDER_ATOMIC];
 };
 
+// 연산
+Blockly.JavaScript['Oper_x'] = function(block) {
+  var value_a = Blockly.JavaScript.valueToCode(block, 'A', Blockly.JavaScript.ORDER_ATOMIC);
+  var dropdown_name = block.getFieldValue('NAME');
+  var value_b = Blockly.JavaScript.valueToCode(block, 'B', Blockly.JavaScript.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = `\n`;
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_ATOMIC];
+};
 
 //// set_yticklabels
 Blockly.JavaScript['set_yticklabels'] = function (block) {
