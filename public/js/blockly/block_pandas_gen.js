facaa54402a2a7aad624742d7498d9e25ac94089
dinb1242 <39480072+dinb1242@users.noreply.github.com>
2021-01-29 09:20:24 +0900

    drop 살짝 수정

--- public/js/blockly/block_pandas_gen.js
+++ public/js/blockly/block_pandas_gen.js
@@ -587,7 +587,7 @@ Blockly.Python['pandas_drop'] = function(block) {
   var value_name3 = Blockly.Python.valueToCode(block, 'd3', Blockly.Python.ORDER_ATOMIC);
   var value_name4 = Blockly.Python.valueToCode(block, 'd4', Blockly.Python.ORDER_ATOMIC);
   // TODO: Assemble Python into code variable.
-  var code = `${value_name1} = ${value_name2}.drop(index = ${value_name3}, axis = ${value_name4})\n`
+  var code = `${value_name1} = ${value_name2}.drop(columns = ${value_name2}.columns[${value_name3}], axis = ${value_name4})\n`
   // TODO: Change ORDER_NONE to the correct strength.
   return code;
 };  
