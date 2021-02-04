8091ddc4144c5b088e9f19ca944873dade2ec7e6
Bantree96 <51081373+Bantree96@users.noreply.github.com>
2021-02-04 11:46:44 +0900

    사이파이 파일생성

--- public/js/blockly/block_pyodide_gen.js
+++ public/js/blockly/block_pyodide_gen.js
@@ -396,66 +396,7 @@ Blockly.Python['stratifiedkfold'] = function (block) {
     return code;
 };
 
-Blockly.Python['import_scipy'] = function (block) {
-    var dropdown_scipy = block.getFieldValue('scipy');
-    // TODO: Assemble Python into code variable.
-    var code = '\n';
-    switch (dropdown_scipy) {
-        case "curve_fit":
-            code = "from scipy.optimize import curve_fit";
-            break;
-        case "interp1d":
-            code = "from scipy.interpolate import interp1d";
-            break;
-        case "UnivariateSpline":
-            code = "from scipy.interpolate import UnivariateSpline";
-            break;
-        case "quad":
-            code = "from scipy.integrate import quad";
-            break;
-        case "trapz":
-            code = "from scipy.integrate import trapz";
-            break;
-        case "wavfile":
-            code = "from scipy.io import wavfile";  
-            break;
-        case "signal":
-            code = "from scipy import signal";  
-            break;
-        case "ndimage":
-            code = "from scipy import ndimage as ndi";  
-            break;
-    }
 
-    return code + '\n';
-};
-Blockly.Python['curve_fit'] = function(block) {
-    var value_fun = Blockly.Python.valueToCode(block, 'fun', Blockly.Python.ORDER_ATOMIC);
-    var value_x = Blockly.Python.valueToCode(block, 'x', Blockly.Python.ORDER_ATOMIC);
-    var value_y = Blockly.Python.valueToCode(block, 'y', Blockly.Python.ORDER_ATOMIC);
-    // TODO: Assemble Python into code variable.
-    var code ='curve_fit('+value_fun+', '+value_x+', '+value_y+')';
-    // TODO: Change ORDER_NONE to the correct strength.
-    return [code, Blockly.Python.ORDER_NONE];
-  };
-  Blockly.Python['univariatespline'] = function(block) {
-    var value_x = Blockly.Python.valueToCode(block, 'x', Blockly.Python.ORDER_ATOMIC);
-    var value_y = Blockly.Python.valueToCode(block, 'y', Blockly.Python.ORDER_ATOMIC);
-    var text_s = block.getFieldValue('s');
-    // TODO: Assemble Python into code variable.
-    var code = 'UnivariateSpline('+value_x+', '+value_y+', s='+text_s+')';
-    // TODO: Change ORDER_NONE to the correct strength.
-    return [code, Blockly.Python.ORDER_NONE];
-  };
-  Blockly.Python['quad'] = function(block) {
-    var value_x = Blockly.Python.valueToCode(block, 'x', Blockly.Python.ORDER_ATOMIC);
-    var text_s = block.getFieldValue('s');
-    var text_e = block.getFieldValue('e');
-    // TODO: Assemble Python into code variable.
-    var code = 'quad('+value_x+', '+text_s+', '+text_e+')';
-    // TODO: Change ORDER_NONE to the correct strength.
-    return [code, Blockly.Python.ORDER_NONE];
-  };
   Blockly.Python['import_tree'] = function(block) {
     var dropdown_tree = block.getFieldValue('tree');
     // TODO: Assemble Python into code variable.
---
+++ public/js/blockly/block_scipy.js
@@ -0,0 +1,78 @@
+// import사이파이
+Blockly.Blocks['import_scipy'] = {
+    init: function () {
+        this.appendDummyInput().appendField(Blockly.Msg.SCIPY_IMPORT).appendField(new Blockly.FieldDropdown([
+            [
+                "curve_fit", "curve_fit"
+            ],
+            [
+                "interp1d", "interp1d"
+            ],
+            [
+                "UnivariateSpline", "UnivariateSpline"
+            ],
+            [
+                "quad", "quad"
+            ],
+            [
+                "trapz", "trapz"
+            ],
+            [
+                "wavfile", "wavfile"
+            ],
+            [
+                "signal", "signal"
+            ],
+            [
+                "ndimage", "ndimage"
+            ]  
+
+        ]), "scipy");
+        this.setInputsInline(true);
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour("%{BKY_SCIPY_HUE}");
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+
+
+Blockly.Blocks['curve_fit'] = {
+    init: function () {
+        this.appendValueInput("fun").setCheck(null).appendField(Blockly.Msg.SCIPY_CURVE_FIT);
+        this.appendValueInput("x").setCheck(null).appendField("x_data");
+        this.appendValueInput("y").setCheck(null).appendField("y_data");
+        this.setInputsInline(true);
+        this.setOutput(true, null);
+        this.setColour("%{BKY_SCIPY_HUE}");
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+
+
+Blockly.Blocks['univariatespline'] = {
+    init: function () {
+        this.appendValueInput("x").setCheck(null).appendField("[UnivariateSpline] x_data");
+        this.appendValueInput("y").setCheck(null).appendField("Y_data");
+        this.appendDummyInput().appendField(new Blockly.FieldTextInput("1"), "s");
+        this.setInputsInline(true);
+        this.setOutput(true, null);
+        this.setColour("%{BKY_SCIPY_HUE}");
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
+Blockly.Blocks['quad'] = {
+    init: function () {
+        this.appendValueInput("x").setCheck(null).appendField(Blockly.Msg.SCIPY_QUAD);
+        this.appendDummyInput().appendField(new Blockly.FieldTextInput("0"), "s");
+        this.appendDummyInput().appendField(new Blockly.FieldTextInput("1000"), "e");
+        this.setInputsInline(true);
+        this.setOutput(true, null);
+        this.setColour("%{BKY_SCIPY_HUE}");
+        this.setTooltip("");
+        this.setHelpUrl("");
+    }
+};
