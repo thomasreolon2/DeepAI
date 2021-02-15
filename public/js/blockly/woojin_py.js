564108e040427518f75e706a8d6a9f73208b3120
남지원 <51081373+Bantree96@users.noreply.github.com>
2021-02-15 23:20:30 +0900

    Revert "123"
    This reverts commit 5e7a70a5158319d9ec9bac94dbb124af1bdb898e, reversing
    changes made to 5e2e457b98b5ef7c8a19dbdbdc78022901fe5c35.

--- public/js/blockly/woojin_py.js
+++ public/js/blockly/woojin_py.js
@@ -1,3 +1,77 @@
+
+//db
+
+//자동 커밋 (con).isolation_level = None  
+
+Blockly.Python['db_autocommit'] = function (block) {
+  var variable_list1 = Blockly.Python.variableDB_.getName(block.getFieldValue('list1'), Blockly.Variables.NAME_TYPE);
+  // TODO: Assemble JavaScript into code variable.
+  var code = `${variable_list1}.isolation_level = None\n`;
+  // TODO: Change ORDER_NONE to the correct strength.
+  return code;
+};
+
+
+//DB삭제 DROP TABLE IF EXISTS (테이블)
+
+Blockly.Python['db_deletedb'] = function (block) {
+  var variable_list1 = Blockly.Python.variableDB_.getName(block.getFieldValue('list1'), Blockly.Variables.NAME_TYPE);
+  var indata1 = Blockly.Python.valueToCode(block, 'SET0', Blockly.Python.ORDER_ATOMIC);
+  // TODO: Assemble JavaScript into code variable.
+  var code = `${variable_list1} = "DROP TABLE IF EXISTS ${indata1}"\n`;
+  // TODO: Change ORDER_NONE to the correct strength.
+  return code;
+};
+
+
+Blockly.Python['db_select6'] = function (block) {
+  var variable_list = Blockly.Python.variableDB_.getName(block.getFieldValue('list1'), Blockly.Variables.NAME_TYPE);
+  var value_SET0 = Blockly.Python.valueToCode(block, 'SET0', Blockly.Python.ORDER_ATOMIC);
+  var value_SET1 = Blockly.Python.valueToCode(block, 'SET1', Blockly.Python.ORDER_ATOMIC);
+  var value_SET2 = Blockly.Python.valueToCode(block, 'SET2', Blockly.Python.ORDER_ATOMIC);
+  var dropdown_name = block.getFieldValue('NAME');
+  // TODO: Assemble JavaScript into code variable.
+  var code = `${variable_list} = "SELECT ${value_SET0} FROM ${value_SET1} ${dropdown_name} ${value_SET2}"\n`;
+  return code;
+};
+
+
+//고장 진단
+
+Blockly.Python['scipy_bartlett'] = function(block) {
+  var value_name2 = Blockly.Python.valueToCode(block, 'd2', Blockly.Python.ORDER_ATOMIC);
+  // TODO: Assemble Python into code variable.
+  var code = `signal.bartlett(${value_name2})`;
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.Python.ORDER_ATOMIC];
+};
+
+Blockly.Python['scipy_tsearch'] = function(block) {
+  var value_name2 = Blockly.Python.valueToCode(block, 'd2', Blockly.Python.ORDER_ATOMIC);
+  // TODO: Assemble Python into code variable.
+  var code = `spatial.tsearch(${value_name2})`;
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.Python.ORDER_ATOMIC];
+};
+
+
+Blockly.Python['scipy_distance_matrix_minkowski_distance_minkowski_distance_p'] = function(block) {
+  var value_name2 = Blockly.Python.valueToCode(block, 'd2', Blockly.Python.ORDER_ATOMIC);
+  var dropdown_dic = block.getFieldValue('scipy');
+  // TODO: Assemble Python into code variable.
+  var code = `spatial.${dropdown_dic}(${value_name2})`;
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.Python.ORDER_ATOMIC];
+};
+
+Blockly.Python['scipy_procrustes'] = function(block) {
+  var value_name2 = Blockly.Python.valueToCode(block, 'd2', Blockly.Python.ORDER_ATOMIC);
+  // TODO: Assemble Python into code variable.
+  var code = `spatial.procrustes(${value_name2})`;
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.Python.ORDER_ATOMIC];
+};
+
 // 사용하지 않는 블럭
 // //조회 sql = “SELECT (필드) FROM (테이블) ORDER BY (필드)”
 
