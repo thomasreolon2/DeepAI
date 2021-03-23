acaf6d16d7918597679f142cda3ebed321b90a0d
Bantree96 <51081373+Bantree96@users.noreply.github.com>
2021-03-23 09:27:49 +0900

    Revert "LTI Test2"
    This reverts commit 2c87b2b11dba259c787d8bced1b8f2f066c49baf.

--- server.js
+++ server.js
@@ -2,114 +2,81 @@
 // server.js가 node에서 사용하는 통상적인 app.js임
 
 // express를 활용한 서버 만들기
-/// sj -> const express = require('express'); // 익스프레스 모듈 require 및 app 설정 (해당 방식은 express 에서 사용하도록 한 규약? 같은 방식)
-/// sj -> const fs = require('fs'); // 파일 시스템 
+// const express = require('express'); // 익스프레스 모듈 require 및 app 설정 (해당 방식은 express 에서 사용하도록 한 규약? 같은 방식)
+const fs = require('fs'); // 파일 시스템 
 //var http = require('http'); // 이건 왜 주석처리 되있던 걸까?
 const ejs = require("ejs"); // ejs는 서버에서 JS로 템플릿을 만들 수 있게 도와준다.
 
-//// -> sj var app = express();
-
-/////// LTI
-
-
-var express = require('express');
-
-var cfenv = require('cfenv');
-
-var uuid = require("uuid4");
-var lti = require("ims-lti");
-
-var fs = require('fs');
-// var mod2File = fs.readFileSync(".html", "utf8");
-var mod3File = fs.readFileSync("BlockExport_Editor.html", "utf8");
-
-// create a new express server
+const bodyParser = require('body-parser');
+const express = require('express');
+const session = require('express-session');
+const lti = require('./lti');             // lit 파일로 가서, 사용자 이메일 등등 정보 받고 다시 넘어옴.
+//const port = process.env.PORT || 3000;
+// this express server should be secured/hardened for production use
+//const app = express();
 var app = express();
 
-app.set('trust proxy', function (ip) {
-  if (ip === '127.0.0.1' || ip === '114.71.137.154') return true // trusted IPs
-  else return false
-})
-
-var sessions = {};
-console.log(__dirname + "/public");
-app.use(express.static(__dirname + '/public'));
-
-app.post("*", require("body-parser").urlencoded({extended: false}));
-app.post("./BlockExport_Editor.html", (req, res) => {	
-	var moodleData = new lti.Provider("top", "secret");
-	moodleData.valid_request(req, (err, isValid) => {
-		if (!isValid) {
-			res.send("Invalid request: " + err);
-			return ;
-		}
-		
-		var sessionID = uuid();
-		sessions[sessionID] = moodleData;
-	
-		var sendMe = mod3File.toString().replace("//PARAMS**GO**HERE",
-				`
-					var params = {
-						sessionID: "${sessionID}",
-						user: "${moodleData.body.ext_user_username}"
-					};
-				`);
-
-		res.setHeader("Content-Type", "text/html");
-		res.send(sendMe);
-	});   // moodleDate.valid_request
-	
-});       // app.post("/module_3");
 
 
-app.get("/grade/:sessionID/:grade", (req, res) => {
-	const session = sessions[req.params.sessionID];
-	var grade = req.params.grade;
-	var resp;
-	
-	if (grade < 60) {
-		resp = `${grade} is too low. How about sixty instead?`;
-		grade = 60;
-	} else if (grade > 90) {
-		resp = `${grade} is too high. How about ninety instead?`;
-		grade = 90;		
- 	} else {
- 		resp = `${grade} sounds reasonable, sure.`;
- 	}
-	
-	session.outcome_service.send_replace_result(grade/100, (err, isValid) => {
-		if (!isValid)
-			resp += `<br/>Update failed ${err}`;
+app.set('view engine', 'pug');
+// memory store shouldn't be used in production
+app.use(session({
+  secret: process.env.SESSION_SECRET || 'dev',
+  resave: false,
+  saveUninitialized: true,
+}));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
 
-		res.send(resp);
-	});
+app.set('json spaces', 2);
 
-});    // app.get("/grade...")
+app.enable('trust proxy');
 
-var appEnv = cfenv.getAppEnv();
-appEnv.port = 50197   ////////////일단은 이렇게
-// start server on the specified port and binding host
-var server = app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.port);
+
+app.get('/', (req, res, next) => {
+ 
+  return res.sendFile(__dirname + "/BlockExport_Editor.html");
 });
-///////
+
+// 로그인 한 사용자에 대한 이름과 일부 정보를 표시하는 보호 된 페이지의 데모
+app.get('/application', (req, res, next) => {
+  
+  if (req.session.userId) {
+    
+    console.log(req.session.userId);
+    console.log(req.session.ltiConsumer);
+    console.log(req.session.username);
+
+    return res.render('index', {
+      email: req.session.email,
+      username: req.session.username,
+      ltiConsumer: req.session.ltiConsumer,
+      userId: req.session.userId,
+      isTutor: req.session.isTutor,
+      context_id: req.session.context_id
+    })
+  } else {
+    next(new Error('Session invalid. Please login via LTI to use this application.'));
+  }
+});
+
+
+app.post('/launch_lti', lti.handleLaunch);
 
 const options = {
   key: fs.readFileSync('./keys/private.pem'),
   cert: fs.readFileSync('./keys/public.pem')
 };
 
-// var server = require('http').createServer(options, app).listen(50197, function(){
-//     console.log('Socket IO server listening on port 50197');
-// });
-
+var server = require('http').createServer(options, app).listen(50197, function(){
+    console.log('Socket IO server listening on port 50197');
+});
 
 
 // 명성이형이 하던 크롤링
 const cheerio = require("cheerio")
 const client = require('cheerio-httpcli');  
-const bodyParser = require('body-parser'); 
+// const bodyParser = require('body-parser'); 
 const url = require('url');   
 const request = require('request'); 
 
@@ -126,9 +93,9 @@ app.use('/js', express.static(__dirname + '/public/js'));
 app.use(express.static(__dirname + '/public')); 
 
 //index 
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + "/BlockExport_Editor.html");
-}); 
+// app.get('/', function (req, res) {
+//     res.sendFile(__dirname + "/BlockExport_Editor.html");
+// }); 
 
 app.get('/cnn', function (req, res) {
     res.sendFile(__dirname + "/cnnTest.html");
