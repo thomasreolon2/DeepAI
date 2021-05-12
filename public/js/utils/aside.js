a6355610dd8a7804dba5527fc29bd88a0279cacd
Bantree96 <51081373+Bantree96@users.noreply.github.com>
2021-05-12 10:35:55 +0900

    js파일로 분리작업

--- public/js/utils/aside.js
+++ public/js/utils/aside.js
@@ -33,40 +33,4 @@ function sidebar_clear() {
     $("#graph1").after("<div id='csv_show'></div>");
 };
 
-    //===================================
-    // 커스텀 prompt
-    //===================================
-
-    const prompt2 = (title, defaultInput, button1, button2) => { return new Promise(function (resolve, reject) {
-      let tempData = null;
-
-      $.MessageBox({ // 커스텀 메세지 박스
-        buttonDone  : button1, // 버튼
-        buttonFail  : button2,
-        message     : title, 
-        input       : defaultInput, // 입력창, checkEvent : 기본입력 값 -> 쿠키에 저장된 값
-        speed       : 0
-      })
-      .done(function(data){ // button1일때
-        if($.trim(data)){
-          resolve(checkFileName(data));
-        }
-      }).fail(function(){ // button2일때
-        resolve(null);
-      });
-    })};
-
-
-
-  // 파일명에 .py, .xml이 들어갔을 경우 제거
-  function checkFileName(FileName) {
-    var _cNameLen = FileName.length;                                      // 파일명의 길이
-    var _lastDot = FileName.lastIndexOf('.');                            // 파일명에서 가장 마지막에 나오는 .확인 => 확장자 체크
-    var _fileExt = FileName.substring(_lastDot, _cNameLen).toLowerCase(); // 확장자를 소문자로 변경해 저장
-
-    if(_fileExt == ".xml" || _fileExt == ".py" ){
-      FileName = FileName.substring(0,_lastDot);
-    }
-
-    return FileName;
-  }
+    
---
+++ public/js/utils/libraryAdd.js
@@ -0,0 +1,97 @@
+// 각 카테고리 ID
+$('div[aria-posinset="13"]').hide(); 	//	num
+$('div[aria-posinset="14"]').hide(); 	//	pan
+$('div[aria-posinset="15"]').hide(); 	//  mat
+$('div[aria-posinset="16"]').hide(); 	//	Scipy
+$('div[aria-posinset="17"]').hide(); 	//	Scikit-Image
+$('div[aria-posinset="18"]').hide();	//	Scikit-learn
+
+// 로딩창 생성
+function showLibLoading() {
+    $("#loading_image1").show();
+    $("#runButton1").css({
+        'display': "none"
+    });
+    $("#loading_image2").show(); // 로딩 끝나면 로딩 창 없앰 
+    $("#runButton2").css({
+        'display': "none"
+    });
+}
+
+// 로딩창 제거
+function hideLibLoading() {
+    $("#loading_image1").hide(); // 로딩 끝나면 로딩 창 없앰 
+	$("#runButton1").css({
+		'display': "block"
+	});
+    $("#loading_image2").hide(); // 로딩 끝나면 로딩 창 없앰 
+	$("#runButton2").css({
+		'display': "inline"
+	});
+}
+
+ /////////////////////////////////
+ // 라이브러리 추가 버튼 동작
+ /////////////////////////////////
+
+ // 데이터 사이언스
+
+
+ // $("#block_type_btn").on("click", function () {
+ //   visibleAll();
+ //   $(".btn-group .codebBtn").eq(2).remove()
+ //   if (projectType == 0) {
+ //     projectType = 1
+ //   }
+ //   $(".ui.modal").modal("hide");
+ // })
+ // $("#code_type_btn").on("click", function () {
+ //   visibleCode();
+ //   $(".btn-group").remove()
+ //   if (projectType == 0) {
+ //     projectType = 2
+ //   }
+ //   $(".ui.modal").modal("hide");
+ // })
+
+$("#ds_import_btn").on("click", function () {
+	showLibLoading();
+
+	$("#ds_import_btn").removeClass("blue").addClass("green");
+	$("#ds_import_btn").text("추가됨");
+
+	pyodide.loadPackage(['pandas', 'matplotlib', 'numpy']).then(() => {
+		flagMatplotlibImport = 1;
+		$('div[aria-posinset="13"]').show();
+		$('div[aria-posinset="14"]').show();
+		$('div[aria-posinset="15"]').show();
+		hideLibLoading();
+	})
+});
+
+// 머신러닝
+$("#ml_import_btn").on("click", function () {
+	showLibLoading();
+
+	$("#ml_import_btn").removeClass("blue").addClass("green");
+	$("#ml_import_btn").text("추가됨");
+
+	pyodide.loadPackage(['scipy', 'scikit-learn']).then(() => {
+		$('div[aria-posinset="16"]').show();
+		$('div[aria-posinset="18"]').show();
+		hideLibLoading();
+	})
+});
+
+// 이미지 프로세싱
+$("#image_processing_import_btn").on("click", function () {
+	showLibLoading();
+
+	$("#image_processing_import_btn").removeClass("blue").addClass("green");
+	$("#image_processing_import_btn").text("추가됨");
+
+	pyodide.loadPackage(['scikit-image']).then(() => {
+		$('div[aria-posinset="17"]').show();
+		hideLibLoading();
+	})
+});
