f3691a746a5869fd4eef0340512de5951f371928
Bantree96 <51081373+Bantree96@users.noreply.github.com>
2021-05-12 10:39:02 +0900

    들여쓰기 정리

--- public/js/utils/aside.js
+++ public/js/utils/aside.js
@@ -1,36 +1,36 @@
 // aside 열고 닫기 토글
 function sidebar_toggle() {
-    var duration = 300;
-    var side = $('#sidebar');
+	var duration = 300;
+	var side = $('#sidebar');
 
-    // side의 클래스를 open으로 토글함
-    side.toggleClass('open');
+	// side의 클래스를 open으로 토글함
+	side.toggleClass('open');
 
-    if (side.hasClass('open')) {
-      side.stop(true).animate({
-        right: '0px'
-      }, duration);
-    } else {
-      side.stop(true).animate({
-        right: '-900px'
-      }, duration);
-    };
+	if (side.hasClass('open')) {
+		side.stop(true).animate({
+		right: '0px'
+	}, duration);
+	} else {
+		side.stop(true).animate({
+		right: '-900px'
+		}, duration);
+	};
 };
 
 // 지우기(상단지우기)
 function csv_clear() {
-  $("#graph1").remove(); 
-  $("#csv_show").before("<div id='graph1' style=\"height:500px; width:900;\"><img id='pyplotfigure' /></div>");
-  // $('#csv_show').empty(); // 초기화
+	$("#graph1").remove(); 
+	$("#csv_show").before("<div id='graph1' style=\"height:500px; width:900;\"><img id='pyplotfigure' /></div>");
+  	// $('#csv_show').empty(); // 초기화
 };
 
 // 전체초기화
 function sidebar_clear() {
-    //$('#graph1').empty(); 기존 그래프 초기화
-    $("#graph1").remove(); 
-    $("#csv_show").before("<div id='graph1' style=\"height:500px; width:900;\"><img id='pyplotfigure' /></div>");
-    $("#csv_show").remove(); 
-    $("#graph1").after("<div id='csv_show'></div>");
+	//$('#graph1').empty(); 기존 그래프 초기화
+	$("#graph1").remove(); 
+	$("#csv_show").before("<div id='graph1' style=\"height:500px; width:900;\"><img id='pyplotfigure' /></div>");
+	$("#csv_show").remove(); 
+	$("#graph1").after("<div id='csv_show'></div>");
 };
 
-    
+	
--- server.js
+++ server.js
@@ -9,12 +9,12 @@ const ejs = require("ejs"); // ejs는 서버에서 JS로 템플릿을 만들 수
 var app = express();
 
 const options = {
-  key: fs.readFileSync('./keys/private.pem'),
-  cert: fs.readFileSync('./keys/public.pem')
+	key: fs.readFileSync('./keys/private.pem'),
+	cert: fs.readFileSync('./keys/public.pem')
 };
 
 var server = require('http').createServer(options, app).listen(60823, function(){
-    console.log('Socket IO server listening on port 60823');
+	console.log('Socket IO server listening on port 60823');
 });
 
 
@@ -39,60 +39,60 @@ app.use(express.static(__dirname + '/public'));
 
 //index 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + "/BlockExport_Editor.html");
+	res.sendFile(__dirname + "/BlockExport_Editor.html");
 }); 
 
 app.get('/cnn', function (req, res) {
-    res.sendFile(__dirname + "/cnnTest.html");
+	res.sendFile(__dirname + "/cnnTest.html");
 });
 
 app.get("/test", function (req, res) { // postTest라는 주소로 POST요청이 들어오면 실행
-    res.render(__dirname + "/test.ejs");
+	res.render(__dirname + "/test.ejs");
 }); 
 
 app.get('/datasets_load_lms', function (req, res) {  //csv 파일 읽기 
-  res.sendFile(__dirname + "/datasets_load_lms.html"); 
+	res.sendFile(__dirname + "/datasets_load_lms.html"); 
 });       
 
 app.get('/show_chart', function (req, res) {  //차트 보기  
-  res.sendFile(__dirname + "/show_chart.html");
+	res.sendFile(__dirname + "/show_chart.html");
 });   
 // POST Test 
 app.post("/postTest", function (req, res) { // postTest라는 주소로 POST요청이 들어오면 실행
 
-    console.log(req.body); // body에 있는 정보를 콘솔창에 출력.  
-    res.json({ok: true}); // 클라이언트에 성공했다고 신호를 보냄.
+	console.log(req.body); // body에 있는 정보를 콘솔창에 출력.  
+	res.json({ok: true}); // 클라이언트에 성공했다고 신호를 보냄.
 
 });  
 
 //socket io 부분 
 // connection이 수립되면 event handler function의 인자로 socket인 들어온다
 io.on('connection', function (socket) { 
-    var get_html; 
+	var get_html; 
 
-    socket.on('Scripts_Get_URL', function (data) {
-      console.log(data.S_option); 
-      get_html = scripts(data.S_option); 
-    });   
+	socket.on('Scripts_Get_URL', function (data) {
+		console.log(data.S_option); 
+		get_html = scripts(data.S_option); 
+	});   
 
-    socket.emit('Scripts_Get_URL', get_html);  
+	socket.emit('Scripts_Get_URL', get_html);  
 });
 
 // express라우팅 : 엔드포인트(URI)의 정의, 그리고 URI가 클라잉너트 요청에 응답하는 방식
 app.post("/test2", function (req, res) { 
-  console.log("페이지 정보 가져오기");
-  let url = req.body.url // 클라이언트로 받은 url 정보
-  console.log(req.body.url); // url 한번 콘솔에 찍어봄
-  let result = ""; //getHTML(); 
-  async function getForum() {
-    try {
-      let response = await axios.get(
-        url         // url을 가지고 axios.get을 통해 html dom을 가져옴
-      );
-      res.json({data: response.data}); // 클라이언트에 값을 보냄.
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  result = getForum();  
+	console.log("페이지 정보 가져오기");
+	let url = req.body.url // 클라이언트로 받은 url 정보
+	console.log(req.body.url); // url 한번 콘솔에 찍어봄
+	let result = ""; //getHTML(); 
+	async function getForum() {
+		try {
+			let response = await axios.get(
+				url         // url을 가지고 axios.get을 통해 html dom을 가져옴
+			);
+			res.json({data: response.data}); // 클라이언트에 값을 보냄.
+		} catch (error) {
+			console.error(error);
+		}
+	}
+	result = getForum();  
 }); 
